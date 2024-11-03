76f7e7e1ce58319bdc834665b873ffb15e3b10b8
Samuel Fiais <samuelfiais.dev@gmai.com>
2024-11-03 19:25:00 -0300

    [REFACTOR] remove unused imports and comments from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { Header } from "./components/Header";
-import { Card } from "./components/Card";
 import { SkillsPage } from "./components/SkillsPage";
-import { Carousel } from "./components/Carousel";
-import { Button } from "./components/Button";
 import { ProjectsPage } from "./components/ProjectsPage";
 import { ContactsPage } from "./components/ContactsPage";
 import { Footer } from "./components/Footer";
 
 export const App = () => {
-  // Portfolio for full-stack developer, Samuel Fiais, using Neobrutalism design
   return (
     <div className="App flex flex-col font-monoespace scroll-smooth bg-amber-400 selection:bg-black selection:text-white">
       <Header />
